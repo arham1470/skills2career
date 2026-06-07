eaf77b05f9e580a2550211525079ca232518da9b
ARHAM-ACA <arham1470@gmail.com>
2026-06-07 23:15:44 +0530

    Configure Brevo SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,20 +1,39 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   try {
-    const result = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: options.email,
-      subject: options.subject,
-      html: options.html || options.message,
+
+    console.log("EMAIL_USER =", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS_EXISTS =", !!process.env.EMAIL_APP_PASSWORD);
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_APP_PASSWORD,
+      },
     });
 
-    console.log("Email sent:", result);
+    // Define the email options
+    const mailOptions = {
+      from: `CareerBridge <${process.env.EMAIL_USER}>`,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+      html: options.html,
+      attachments: options.attachments || [],
+    };
+
+    await transporter.verify();
+    console.log("SMTP Connection Successful");
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Email sent successfully to ${options.email}: ${info.messageId}`);
     return true;
   } catch (error) {
-    console.error("Email error:", error);
+    console.error("Error sending email:", error);
     return false;
   }
 };
