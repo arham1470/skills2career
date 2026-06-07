eaea6c6a8a1cd04ef88f4ddc8e33d484b203324e
ARHAM-ACA <arham1470@gmail.com>
2026-06-07 21:44:57 +0530

    Test SMTP connection

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,11 +7,16 @@ const sendEmail = async (options) => {
     console.log("EMAIL_PASS_EXISTS =", !!process.env.EMAIL_APP_PASSWORD);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
       },
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
     });
 
     // Define the email options
@@ -24,6 +29,8 @@ const sendEmail = async (options) => {
       attachments: options.attachments || [],
     };
 
+    await transporter.verify();
+    console.log("SMTP Connection Successful");
     // Send the email
     const info = await transporter.sendMail(mailOptions);
     console.log(`Email sent successfully to ${options.email}: ${info.messageId}`);
